9ff314b4485b5f19535030dc5a6fd27fc0fa9638
yanghongchen <351338974@qq.com>
2016-01-12 22:47:46 +0800

    新增bootstrap框架支持

---
+++ 03 time/003_time.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>这个是一个时间函数</title>
+	<link rel="stylesheet" href="../static/bootstrap/css/bootstrap.min.css"/>
+	<script type="text/javascript" src="http://cdn.bootcss.com/jquery/1.11.3/jquery.min.js"></script>
+	<script src="../static/bootstrap/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<div class="container">
+		<h2>日期和时间</h2>
+		<hr>
+		<div class="container">
+			显示，操作日期和时间一开始看起来简单，但随着用户复杂性和多样性的增加这些处理任务也会变得困难起来。
+			你的用户有可能来自不同的地区。
+		</div>
+	</div>
+</body>
+</html>
